5e17aa21ee8b940e65f0befbd2c88e07128fc63f
Nathan Machoka <natemachoka@gmail.com>
2024-10-11 07:40:25 -0700

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function to_str that takes a float n as argument
+returns the string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a floating-point number.
+
+    Args:
+        n (float): The floating-point number.
+
+    Returns:
+        str: The string representation of the number.
+    """
+    return str(n)
